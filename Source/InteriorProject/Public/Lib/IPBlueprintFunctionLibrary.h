42b382c41476cdae3f52d1cb8200247b0f6b25ef
Tan Wei Yang <weiyang.tan@ecoworld.local>
2019-12-27 17:16:21 +0800

    Added CPP Classes

---
+++ Source/InteriorProject/Public/Lib/IPBlueprintFunctionLibrary.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "IPBlueprintFunctionLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INTERIORPROJECT_API UIPBlueprintFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/InteriorProject/Public/Settings/IPWorldSettings.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/WorldSettings.h"
+#include "IPWorldSettings.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INTERIORPROJECT_API AIPWorldSettings : public AWorldSettings
+{
+	GENERATED_BODY()
+	
+};
