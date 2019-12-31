275bb6824adb7de7344333c0f2c48c3abfd361a5
Tan Wei Yang <weiyang.tan@ecoworld.local>
2019-12-31 18:20:34 +0800

    added Pawn Movement

--- Source/InteriorProject/Private/Player/IPPawn.cpp
+++ Source/InteriorProject/Private/Player/IPPawn.cpp
@@ -3,19 +3,43 @@
 
 #include "Player/IPPawn.h"
 
+#include <GameFramework/PawnMovementComponent.h>
+#include <GameFramework/FloatingPawnMovement.h>
+#include <GameFramework/PlayerInput.h>
+#include <GameFramework/PlayerController.h>
+#include <Components/SphereComponent.h>
+
 // Sets default values
 AIPPawn::AIPPawn()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	SpawnCollisionHandlingMethod = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+
+	CollisionComponent = CreateDefaultSubobject<USphereComponent>(TEXT("Sphere Collision Component"));
+	CollisionComponent->InitSphereRadius(35.0f); 
+
+	CollisionComponent->CanCharacterStepUpOn = ECB_No;
+	CollisionComponent->SetShouldUpdatePhysicsVolume(true);
+	CollisionComponent->SetCanEverAffectNavigation(false);
+	CollisionComponent->bDynamicObstacle = true;
+
+	RootComponent = CollisionComponent;
+
+	MovementComponent = CreateDefaultSubobject<UPawnMovementComponent, UFloatingPawnMovement>(TEXT("Movement Component"));
+	MovementComponent->UpdatedComponent = CollisionComponent;
+
+	BaseLookUpRate = 45.f;
+	MovementSpeed = 45.f;
 }
 
 // Called when the game starts or when spawned
 void AIPPawn::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	AddControllerPitchInput(25.f);
 }
 
 // Called every frame
@@ -30,5 +54,32 @@ void AIPPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	PlayerInputComponent->BindAxis("MoveForward", this, &AIPPawn::MoveForward);
+	PlayerInputComponent->BindAxis("LookUp", this, &AIPPawn::LookUp);
+	PlayerInputComponent->BindAxis("Turn", this, &AIPPawn::Turn); 
+}
+
+void AIPPawn::MoveForward(float Scale)
+{
+	if (!FMath::IsNearlyZero(Scale, 0.02f) && !Controller)
+	{
+		return;
+	}
+
+	const FRotator & ControlSpaceRot = GetActorRotation(); 
+	AddMovementInput(FRotationMatrix(ControlSpaceRot).GetScaledAxis(EAxis::X), MovementSpeed * Scale * GetWorld()->GetDeltaSeconds() * CustomTimeDilation);
+}
+
+void AIPPawn::Turn(float Scale)
+{
+}
+
+void AIPPawn::LookUp(float Scale)
+{
+	AddControllerPitchInput(Scale * BaseLookUpRate * GetWorld()->GetDeltaSeconds() * CustomTimeDilation); 
+}
+
+void AIPPawn::RightClickMB()
+{
 }
 
--- Source/InteriorProject/Public/Player/IPPawn.h
+++ Source/InteriorProject/Public/Player/IPPawn.h
@@ -6,6 +6,10 @@
 #include "GameFramework/Pawn.h"
 #include "IPPawn.generated.h"
 
+class UPawnMovementComponent;
+class USphereComponent;
+
+
 UCLASS()
 class INTERIORPROJECT_API AIPPawn : public APawn
 {
@@ -16,14 +20,36 @@ public:
 	AIPPawn();
 
 protected:
-	// Called when the game starts or when spawned
+	//~ Start AActor Interface
 	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+	//~ End AActor Interface
+
+protected:
+	/** DefaultPawn movement component */
+	UPROPERTY(Category = Pawn, VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	UPawnMovementComponent* MovementComponent;
+
+	/** DefaultPawn collision component */
+	UPROPERTY(Category = Pawn, VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	USphereComponent* CollisionComponent;
 
 public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+protected:
+	void MoveForward(float Scale);
+	void Turn(float Scale);
+	void LookUp(float Scale);
+
+	void RightClickMB();
+protected:
+	/** Base lookup rate, in deg/sec. Other scaling may affect final lookup rate. */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Pawn")
+	float BaseLookUpRate; 
+
+	/**  Movement Speed */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Pawn")
+	float MovementSpeed;
 };
